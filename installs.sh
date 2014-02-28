3b871d7909aab870cf4db746805f240b7c6ce20c
Austin Lasota <austinlasota@gmail.com>
2014-02-28 12:54:48 -0500

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -4,16 +4,9 @@
 #Whats new:
 #added the ability to add ppa repositories.
 #script can now detect architecture and adapt accordingly.
+#script can now fetch deb files from external sources, and download the proper packages for that achitecture.
 
 
-#eventually this will be used to properly choose which deb files to fetch.
-#WARNING: if your installed .deb file has no online repository that package won't be updated. 
-#MACHINE_TYPE=`uname -m`
-#if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-#  local arch=64
-#else
-#  local arch=32
-#fi
 
 
 read -p "Do you wish to install from the DEB list or your distros repositories? (deb/repo)?" PROMPT
@@ -21,10 +14,19 @@ if [ "$PROMPT" == "deb" ]; then
   printf "Installing from DEB list.\n";
   printf "WARNING: if your listed .deb files do not have an online repository, those package won't be updated!\n"
   sleep 3
-  #place the URL path to the DEB package here.
   
-  URL='http://path.to/my.deb'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
-  URL='http://path.to/my.deb2'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
+MACHINE_ARCH=`uname -m`
+if [ ${MACHINE_ARCH} == 'x86_64' ]; then
+
+    URL='http://path.to/my64bit.deb'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
+    URL='http://path.to/my64bit.deb'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
+  
+else
+
+    URL='http://path.to/my32bit.deb'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
+    URL='http://path.to/my32bit.deb'; PACKAGE=`mktemp`; wget "$URL" -qO $PACKAGE && sudo dpkg -i $PACKAGE; rm $PACKAGE
+    
+fi
   
 else
   printf "Installing from the repositories.";
